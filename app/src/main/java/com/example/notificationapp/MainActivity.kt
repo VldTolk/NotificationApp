386d4beb7a88cbb407306073a7972cd977b394fe
VldTolk <n-v-d-r@yandex.ru>
2023-01-07 14:23:37 +0400

    test

---
+++ app/src/main/java/com/example/notificationapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.notificationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.notificationapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/notificationapp/ui/main/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.example.notificationapp.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.notificationapp.R
+import com.example.notificationapp.viewModel.MainViewModel
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
